7988dd3d3d63556b7f08cbe49f02cebeab75840a
alejandrovarga <137833421+alejandrovarga@users.noreply.github.com>
2025-06-24 22:14:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function resizeImage() {
+  const img = document.getElementById("myImage");
+  img.style.width = "400px";
+  img.style.height = "300px";
+  img.style.border = "5px solid blue"; // Bonus: Add a blue border
+}
